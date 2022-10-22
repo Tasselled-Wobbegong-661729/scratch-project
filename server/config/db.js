459510682a60051d7447a742a510c4681a8b981b
Dennis <hdennispark@gmail.com>
2022-10-22 14:13:35 -0400

    added db file, webpack still having issues

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = 'mongodb+srv://dennis:park@scratch.lkftk6o.mongodb.net/apps?retryWrites=true&w=majority';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'apps',
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log('mongodb', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
