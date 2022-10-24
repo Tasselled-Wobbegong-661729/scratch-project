791a23bcb249b17ef0d1fba314db37d27f216f9c
rebshess <rebeccashesser@gmail.com>
2022-10-24 10:32:41 -0400

    cleaned up routes and controllers

--- server/server.js
+++ server/server.js
@@ -23,6 +23,10 @@ app.post('/signup', userController.signUp, (req, res) => {
   res.status(200).send(res.locals.newUser);
 });
 
+app.post('/login', userController.login, (req, res) => {
+  res.status(200).send(res.locals.loginToken);
+});
+
 // This will check if user's cookie is saved in browser and user is not set, then log the user out.
 // Happens when you stop your express server after login, your cookie remains saved in the browser.
 
@@ -40,86 +44,3 @@ app.get('/', (req, res) =>
 );
 
 app.listen(port, () => console.log(`Server up and listening on port ${port}`));
-
-// app.use('/api/users', usersRoute);
-
-// app.use(
-//   session({
-//     key: 'user_sid',
-//     secret: 'secret-key',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       expires: 600000,
-//     },
-//   })
-// );
-
-// app.use((req, res, next) => {
-//     if (req.cookies.user_sid && !req.session.user) {
-//       res.clearCookie('user_sid');
-//     }
-//     next();
-//   });
-
-//   // middleware function to check for logged-in users
-//   const sessionChecker = (req, res, next) => {
-//     if (req.session.user && req.cookies.user_sid) {
-//       res.redirect('/dashboard');
-//     } else {
-//       next();
-//     }
-//   };
-
-//   // route for Home-Page
-//   app.get('/', sessionChecker, (req, res) => {
-//     res.redirect('/login');
-//   });
-
-//   // route for user signup
-
-//   // route for user Login
-//   app
-//     .route('/login')
-//     .get(sessionChecker, (req, res) => {
-//       // res.sendFile(__dirname + '/public/login.html');
-//     })
-//     .post(async (req, res) => {
-//       const email = req.body.email,
-//         password = req.body.password;
-
-//       try {
-//         let user = await User.findOne({ email: email }).exec();
-//         if (!user) {
-//           res.redirect('/login');
-//         }
-//         user.comparePassword(password, (error, match) => {
-//           if (!match) {
-//             res.redirect('/login');
-//           }
-//         });
-//         req.session.user = user;
-//         res.redirect('/dashboard');
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     });
-
-//   // route for user's dashboard
-//   app.get('/dashboard', (req, res) => {
-//     if (req.session.user && req.cookies.user_sid) {
-//       res.sendFile(__dirname + '/public/dashboard.html');
-//     } else {
-//       res.redirect('/login');
-//     }
-//   });
-
-//   // route for user logout
-//   app.get('/logout', (req, res) => {
-//     if (req.session.user && req.cookies.user_sid) {
-//       res.clearCookie('user_sid');
-//       res.redirect('/');
-//     } else {
-//       res.redirect('/login');
-//     }
-//   });
--- server/userController.js
+++ server/userController.js
@@ -1,28 +1,21 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('./UserModel');
 const connectDB = require('./config/db');
 
 // connectDB();
 
 const userController = {};
+// const { SECRET = 'secret' } = process.env;
 
 userController.signUp = async (req, res, next) => {
   try {
-    console.log(req.body);
-    const user = new User({
-      email: req.body.email,
-      password: req.body.password,
-    });
-    user.password = await bcrypt.hash(user.password, 10);
-
-    await user.save();
-    const token = 'sampleToken';
-    res.header('x-auth-token', token).send({
-      name: user.name,
-      email: user.email,
-    });
-    const newUser = await User.create(user);
-    res.locals.newUser = newUser;
+    // hash the password
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    // create a new user
+    const user = await User.create(req.body);
+    // send new user as response
+    res.locals.newUser = user;
     return next();
   } catch (err) {
     return next({
@@ -30,6 +23,35 @@ userController.signUp = async (req, res, next) => {
         log: 'Error in userController.createUser',
         status: 400,
         message: { err: 'error in usercontroller.signup' },
+      },
+    });
+  }
+};
+
+userController.login = async (req, res, next) => {
+  try {
+    // check if the user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      // check if password matches
+      const result = await bcrypt.compare(req.body.password, user.password);
+      if (result) {
+        // sign token and send it in response
+        const token = await jwt.sign({ email: user.email });
+        res.locals.loginToken = { token };
+        next();
+      } else if (!result) {
+        res.status(400).json({ error: "password doesn't match" });
+      }
+    } else if (!user) {
+      res.status(400).json({ error: "email doesn't exist" });
+    }
+  } catch (err) {
+    return next({
+      err: {
+        log: 'Error in userController.loginUser',
+        status: 400,
+        message: { err: 'error in usercontroller.login' },
       },
     });
   }
